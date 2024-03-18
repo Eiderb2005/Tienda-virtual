87092bfd8a093b6387a03497295f696cbed591fb
RGH-LS24-20 <RGH-LS24-20@UMD.LOCAL.EDU>
2024-03-18 08:13:55 -0500

    Tienda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TablaComponent } from './tabla/tabla.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:"",
+    pathMatch:"full",
+    redirectTo:"/tabla",
+  },
+  {
+    path:"tabla",
+    component:TablaComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
